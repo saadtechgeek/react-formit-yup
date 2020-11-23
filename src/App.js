853d55fe2b1ddd941332bfbb04c6d5597cf44f62
Saad Shahid <saad.techgeek@gmail.com>
2020-11-23 22:01:28 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import FormikFormWithYep from './FormikFormWithYup';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Login</h2>
+      {/* <FormikForm /> */}
+
+      <FormikFormWithYep />
+    </div>
+  );
+}
+
+export default App;
